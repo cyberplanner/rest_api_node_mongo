b495bef0aace1698be961bcd4625de9b8e860b77
cyberplanner <saleh.gfx@gmail.com>
2017-10-08 19:52:14 +0100

    Configure two routes

---
+++ api/routes/toDoRoutes.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = funcion(app) {
+  var todoList = require('../controllers/toDoController');
+  
+  // ToDo Routes
+  app.route('/tasks')
+      .get(todoList.list_all_tasks)
+      .post(todoList.create_a_task);
+      
+  app.route('/task/:taskId')
+      .get(todoList.read_a_task)
+      .put(todoList.update_a_task)
+      .delete(todoList.delete_a_task);
+};
