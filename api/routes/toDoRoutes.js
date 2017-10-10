459ed925888ae873c6674c225cabbf56ba2a2f5d
cyberplanner <saleh.gfx@gmail.com>
2017-10-10 14:55:44 +0100

    API works

--- api/routes/toDoRoutes.js
+++ api/routes/toDoRoutes.js
@@ -1,14 +1,15 @@
 'use strict';
-module.exports = funcion(app) {
+module.exports = function(app) {
   var todoList = require('../controllers/toDoController');
-  
-  // ToDo Routes
+
+  // toDo Routes
   app.route('/tasks')
-      .get(todoList.list_all_tasks)
-      .post(todoList.create_a_task);
-      
-  app.route('/task/:taskId')
-      .get(todoList.read_a_task)
-      .put(todoList.update_a_task)
-      .delete(todoList.delete_a_task);
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_a_task)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
 };
